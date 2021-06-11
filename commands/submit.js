46f69a6e2f9ff4ec0fe23b63f30781193729ac82
victor <dusartvictor@gmail.com>
2021-06-11 18:24:20 +0200

    Close #12 Congratz message

--- commands/submit.js
+++ commands/submit.js
@@ -2,6 +2,22 @@ exports.submitAnswer = (params) => {
     const msg = params.message;
     const timeBetweenSubmit = 600;
 
+    let team = global.teams.filter(team => msg.author.id in team.players);
+    if (team.length == 0) { 
+      msg.author.send({embed: {
+        title: 'Attention',
+        description: 'Vous n\'appartenez pas encore à une équipe !',
+        fields: [
+          {
+            name: 'Vous vous sentez seul...',
+            value: 'Vous pouvez créer une équipe ou même en rejoindre une!'
+          }
+        ]
+      }});
+      return;
+    }
+    team = team[0];
+
     if (msg.author.id in global.users) {
         const timeBetween = (new Date() - global.users[msg.author.id]["submit"])/1000;
         if (timeBetween <= timeBetweenSubmit) {
@@ -16,4 +32,16 @@ exports.submitAnswer = (params) => {
     }
 
     msg.author.send("vous avez bien submit");
+    congratzMsg(params,team, "1.3");
+}
+
+function congratzMsg(params, team, enigmaNumber) {
+    const generalChanelID = "851361677601275914";
+    params.client.channels.cache.get(generalChanelID).send({
+        embed: {
+            title: 'Félicitations',
+            description: 'Les **' + team.name + '** viennent de résoudre l\'énigme '+ enigmaNumber + "\nIls ont " + team.score + " pts!",
+            color: "#FF8849"
+        }
+    });
 }
